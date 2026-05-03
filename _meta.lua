57a1dba5faa5a7053203effc5b683945b9a51db3
AndyHazz <517731+AndyHazz@users.noreply.github.com>
2026-05-03 12:48:52 +0100

    chore: plugin manifest and editor ignores

---
+++ _meta.lua
@@ -0,0 +1,7 @@
+local _ = require("bookshelf_i18n").gettext
+return {
+    name = "bookshelf",
+    fullname = _("Bookshelf"),
+    description = _([[A skeumorphic home screen for KOReader: hero card for the currently-reading book, plus chip-driven shelves for Recent / Latest / Series / Favourites.]]),
+    version = "0.1.0",
+}
